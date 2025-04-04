176b823a96d7e85263712a3f7748e3d8f2870e2d
AbdulRahman-04 <abdulrahman.81869@gmail.com>
2025-04-04 01:35:25 +0000

    toDo Backend

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,5 +1,11 @@
 import express, { Request, Response } from "express";
 import config from "config";
+import helmet from "helmet";
+import compression from "compression";
+import rateLimit from "express-rate-limit";
+import morgan from "morgan"
+import cors from "cors"
+import mongoSanitize from "express-mongo-sanitize";
 
 // DATABASE CONNECTION
 import "./utils/dbConnect";
@@ -16,6 +22,28 @@ import todoRoute from "./controllers/private/index";
 const app = express();
 const PORT: string = config.get<string>("PORT");
 
+// ✅ Middlewares for Security & Performance
+
+app.use(helmet());
+
+app.use(compression());
+
+app.use(cors())
+
+app.use(morgan("dev"))
+
+
+
+app.use(mongoSanitize());
+
+// ✅ Rate Limiting (Prevent DDoS attacks)
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  max: 100, // 100 requests per window
+  message: "Too many requests from this IP, please try again later!",
+});
+app.use(limiter);
+
 app.use(express.json());
 
 app.get("/", (req: Request, res: Response): void => {
@@ -29,9 +57,6 @@ app.get("/", (req: Request, res: Response): void => {
 
 // Public API's
 app.use("/api/public", userRouter);
-
-
-// app.use(authMiddleware);
 
 // Private Routes with Middleware
 app.use("/api/private", authMiddleware, todoRoute);
